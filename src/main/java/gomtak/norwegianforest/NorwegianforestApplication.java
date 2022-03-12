065166ca1eee2578aef1e9be0ff07566ee6907cc
tak6831 <tak6831@gmail.com>
2022-03-12 19:26:38 +0900

    init

---
+++ src/main/java/gomtak/norwegianforest/NorwegianforestApplication.java
@@ -0,0 +1,13 @@
+package gomtak.norwegianforest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorwegianforestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorwegianforestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gomtak/norwegianforest/NorwegianforestApplicationTests.java
@@ -0,0 +1,13 @@
+package gomtak.norwegianforest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorwegianforestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
